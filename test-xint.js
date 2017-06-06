ed0edc05372e5ca9ba245f299d163270666a3228
mbsiff <mbsiff@gmail.com>
2017-06-06 16:43:57 -0400

    testing add, compareTo

--- test-xint.js
+++ test-xint.js
@@ -4,17 +4,32 @@
 let assert = require('assert');
 let Xint = require('./xint');
 
-var n;
+var m, n, z;
 
-
+// constants
 assert.strictEqual(Xint.ZERO.toBinaryString(), '0',
                     'constant 0 exists as expected');
 assert.strictEqual(Xint.ONE.toBinaryString(), '1',
                     'constant 1 exists as expected');
 
+// formation from literals
 n = Xint.make(37);
-assert.strictEqual(n.toBinaryString(), '100101');
+assert.strictEqual(n.toString(2), '100101');
 
 n = Xint.make('132057142653');
-assert.strictEqual(n.toBinaryString(),
+assert.strictEqual(n.toString(2),
                     '1111010111111001110000001010101111101');
+
+// working with different string formats
+
+
+
+// comparisons
+
+
+// addition
+n = Xint.make('132057142653');
+m = Xint.make('999999999999');
+z = Xint.make('1132057142652');
+assert.strictEqual(Xint.compareTo(Xint.add(n, m), z),
+                    0);
