9c54307df89312403d6b3a151c9664b34f3c9f0b
mbsiff <mbsiff@gmail.com>
2017-06-06 13:14:10 -0400

    streamlining

--- xint.js
+++ xint.js
@@ -2,51 +2,70 @@
   'use strict';
   var BB = require('./bitBlocks');
 
-  let zero = make(0);
-  let one = make(1);
+  const ZERO = makeFromNumber(0);
+  const ONE  = makeFromNumber(1);
 
-  function make(x){
-    let obj = {};
-    let n;
-    if (typeof x === 'number'){
-      let size = 0;
-      let m = x;
-      if (n === 0){
-        let bits = BB.makeBits(1);
-        n = bits;
-      } else {
-        while (m > 0){
-          m = Math.floor(m / 2);
-          size++;
-        }
-        let bits = BB.makeBits(size);
-        m = x;
-        for (let i = 0; i < size; i++){
-          BB.setBit(bits, i, m % 2);
-          m = Math.floor(m / 2);
-        }
-        n = bits;
-      }
-    } else {
-      throw new TypeError("This type of input is not currently supported");
+  function bitsToBinaryString(bits) {
+    let s = '';
+    let n = bits.countBits();
+    for (let i=0; i < n; i++) {
+      s = bits.getBit(i) + s;
     }
-    obj.toBinaryString = function(){
-                        if (n.countBits() === 1 && n.getBit(0) === 0){
-                          return '0';
-                        } else {
-                          let s =  n.toString();
-                          let x = s.split("");
-                          for (let i = x.length - 1; x[i] !== '1'; i--){
-                            x.pop();
-                          }
-                          x.reverse();
-                          return x.join("");
-                        }
-                      };
+    return s;
+  }
+
+  function _make(bits) {
+    let obj = {};
+    obj.toBinaryString = () => bitsToBinaryString(bits);
     return obj;
   }
 
+  function makeFromNumber(n) {
+    if (n === 0) {
+      return _make(BB.makeBits(1));
+    }
+    let size = 0;
+    let m = n;
+    while (m > 0) {
+      m = Math.floor(m / 2);
+      size++;
+    }
+    let bits = BB.makeBits(size);
+    m = n;
+    for (let i = 0; i < size; i++){
+      BB.setBit(bits, i, m % 2);
+      m = Math.floor(m / 2);
+    }
+    return _make(bits);
+  }
+
+  function makeFromString(s) {
+    return null;   // !!!
+  }
+
+  function make(x){
+    if (typeof x === 'number'){
+      if (x % 1 === 0) {
+        if (x === 0) {
+          return ZERO;
+        } else if (x === 1) {
+          return ONE;
+        }
+        if (x > Number.MAX_SAFE_INTEGER) {
+          console.warn('...');
+        }
+        return makeFromNumber(x);
+      } else {
+        // throw ...
+      }
+    } else if (typeof x === 'string') {
+      return makeFromString(x);
+    } else {
+      throw new TypeError("This type of input is not currently supported");
+    }
+  }
+
   exports.make = make;
-  exports.ZERO = zero;
-  exports.ONE = one;
+  exports.ZERO = ZERO;
+  exports.ONE = ONE;
 })((typeof exports === 'undefined') ? this.xint = {} : exports);
