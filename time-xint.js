add6f70ba4d3b202831eafc0cd393af127f9c4d1
dcgentile <dc.gentile18@gmail.com>
2017-06-09 11:18:27 -0400

    update

--- time-xint.js
+++ time-xint.js
@@ -2,25 +2,79 @@
 
 let Xint = require('./xint');
 
-function metaTime(op, x, y, name, reps=1000000) {
-  console.time(name);
+// function metaTime(op, x, y, name, reps=1000000) {
+//   console.time(name);
+//
+//   for(let i = 0; i < reps; i++) {
+//     let z = op(x, y);
+//   }
+//   console.timeEnd(name);
+// }
 
-  for(let i = 0; i < reps; i++) {
-    let z = op(x, y);
+// const LIMIT = 65536;
+// let r0 = Math.floor(Math.random() * LIMIT);
+// let r1 = Math.floor(Math.random() * LIMIT);
+// let xx = Xint.make(r0);
+// let yy = Xint.make(r1);
+// console.log('testing on ' + r0 + ', ' + r1);
+// metaTime((x, y) => x + y, r0, r1, 'built-in +');
+// metaTime(Xint.add, xx, yy, 'xint add');
+// metaTime(Xint.blockAdd, xx, yy, 'block add');
+// metaTime((x, y) => x * y, r0, r1, 'built-in *');
+// metaTime(Xint.mult, xx, yy, 'xint mult');
+// metaTime(Xint.blockMult, xx, yy, 'block mult');
+
+function timerAdd(bitSize, repetitions, sampleSize){
+  let tbit = tblock = 0;
+  for (let j = 0; j < sampleSize; j++){
+    let timeBit = timeBlock = 0;
+    let x = Xint.rand(bitSize);
+    let y = Xint.rand(bitSize);
+    let t0 = Date.now();
+
+    for(let i = 0; i < repetitions; i++){
+      let z = Xint.add(x, y);
+    }
+    timeBit += (Date.now() - t0);
+    t0 = Date.now();
+
+    for(let i = 0; i < repetitions; i++){
+      j = i
+      let z = Xint.blockAdd(x, y);
+    }
+    timeBlock += (Date.now() - t0);
+
+    tbit += timeBit / repetitions;
+    tblock += timeBlock / repetitions;
   }
-  console.timeEnd(name);
+  console.log(bitSize, tbit/sampleSize, tblock/sampleSize);
 }
 
+function timerMult(bitSize, repetitions, sampleSize){
+  let tbit = tblock = 0;
+  for (let j = 0; j < sampleSize; j++){
+    let timeBit = timeBlock = 0;
+    let x = Xint.rand(bitSize);
+    let y = Xint.rand(bitSize);
+    let t0 = Date.now();
 
-const LIMIT = 65536;
-let r0 = Math.floor(Math.random() * LIMIT);
-let r1 = Math.floor(Math.random() * LIMIT);
-let xx = Xint.make(r0);
-let yy = Xint.make(r1);
-console.log('testing on ' + r0 + ', ' + r1);
-metaTime((x, y) => x + y, r0, r1, 'built-in +');
-metaTime(Xint.add, xx, yy, 'xint add');
-metaTime(Xint.blockAdd, xx, yy, 'block add');
-metaTime((x, y) => x * y, r0, r1, 'built-in *');
-metaTime(Xint.mult, xx, yy, 'xint mult');
-metaTime(Xint.blockMult, xx, yy, 'block mult');
+    for(let i = 0; i < repetitions; i++){
+      let z = Xint.mult(x, y);
+    }
+    timeBit += (Date.now() - t0);
+    t0 = Date.now();
+
+    for(let i = 0; i < repetitions; i++){
+      let z = Xint.blockMult(x, y);
+    }
+    timeBlock += (Date.now() - t0);
+
+    tbit += timeBit / repetitions;
+    tblock += timeBlock / repetitions;
+  }
+  console.log(bitSize, tbit/sampleSize, tblock/sampleSize);
+}
+
+for (let i = 1000; i< 3000; i = i + 200){
+  timerMult(i, 20, 20);
+}
